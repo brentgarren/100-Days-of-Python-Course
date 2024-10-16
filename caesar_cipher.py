4fb60af86c63cfda3e8e7bdc856422b0e9d887e5
Jodyrobert Brent Garren <105601437+brentgarren@users.noreply.github.com>
2024-10-16 18:39:11 -0500

    Update caesar_cipher.py
    added brute force method

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,6 +2,7 @@ alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 
 indexnum = ""
 is_true = False
+shift_allowed = True
 
 def encrypt():
     text = input("type your message here:\n".lower())
@@ -21,11 +22,14 @@ def encrypt():
 def decrypt():
     decrypted_message = ""
     text = input("type your message here:\n".lower())
-    shift_allowed = False
-    while shift_allowed == False:  
-        shift = int(input("Type the shift number:\n"))
-        if shift >= 0 and shift <= 26:
-            shift_allowed = True
+    if direction == "decode" or direction == "2":
+        global shift_allowed
+        shift_allowed = False
+    if shift_allowed == False:
+        while shift_allowed == False:  
+            shift = int(input("Type the shift number:\n"))
+            if shift >= 0 and shift <= 26:
+                shift_allowed = True
     shift = int(input("Type the shift number:\n"))
     for char in text:
         indexnum = int((alphabet.index(char)-shift))
@@ -34,15 +38,23 @@ def decrypt():
         decrypted_message += alphabet[indexnum]
     print(decrypted_message)
 
+def bruteforce():
+    shift = 0
+    for num in range(1,25):
+        shift +=1
+        decrypt()
+
 ###### Caeser Cipher ######
 
 while is_true == False:
-    direction = input("Please select a mode:\n1. Encode\n2. Decode\n".lower())
-    if direction == "encode" or direction == 1:
+    direction = input("Please select a mode:\n1. Encode\n2. Decode\n3. Bruteforce\n".lower())
+    if direction == "encode" or direction == "1":
         encrypt()
         is_true = True
-    elif direction == "decode" or direction == 2:
+    elif direction == "decode" or direction == "2":
         decrypt()
         is_true = True
+    elif direction == "bruteforce" or direction == "3":
+        bruteforce()
     else: 
         print(f"Incorrect option!")
