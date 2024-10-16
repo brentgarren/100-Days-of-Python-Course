d525efbdce73e67e8dce6e1bdf15f8338d6b9b92
Jodyrobert Brent Garren <105601437+brentgarren@users.noreply.github.com>
2024-10-16 18:08:12 -0500

    Create caesar_cipher.py
    version1

---
+++ caesar_cipher.py
@@ -0,0 +1,40 @@
+alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+indexnum = ""
+is_true = False
+
+def encrypt():
+    text = input("type your message here:\n".lower())
+    while allowed_shift == False:
+      shift = int(input("Type the shift number:\n"))
+      if shift >= 0 and shift <= 26:
+          shift_allowed = True
+    encrypted_message = ""
+    for char in text.lower():
+        indexnum = alphabet.index(char)
+        if indexnum >= 26:
+            indexnum = indexnum - 26
+        encrypted_message += alphabet[(indexnum+shift)]
+    print(encrypted_message)    
+        
+def decrypt():
+    decrypted_message = ""
+    text = input("type your message here:\n".lower())
+    shift = int(input("Type the shift number:\n"))
+    for char in text:
+        indexnum = int((alphabet.index(char)-shift))
+        if indexnum < 0:
+            indexnum = indexnum + 25
+        decrypted_message += alphabet[indexnum]
+    print(decrypted_message)
+
+while is_true == False:
+    direction = input("Please select a mode:\nEncode\nDecode\n".lower())
+    if direction == "encode":
+        encrypt()
+        is_true = True
+    elif direction == "decode":
+        decrypt()
+        is_true = True
+    else: 
+        print(f"Incorrect option!")
