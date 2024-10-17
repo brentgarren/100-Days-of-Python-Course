5644505ab82fb877a6e985b4abb7806382fc10b3
Jodyrobert Brent Garren <105601437+brentgarren@users.noreply.github.com>
2024-10-16 20:19:44 -0500

    Update caesar_cipher.py
    brute force is working

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,11 +1,12 @@
 alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 indexnum = ""
-is_true = False
-shift_allowed = True
 
-def encrypt():
-    text = input("type your message here:\n".lower())
+def get_message():
+   return str(input("Message: "))
+
+
+def encrypt(text):
     shift_allowed = False
     while shift_allowed == False:  
         shift = int(input("Type the shift number:\n"))
@@ -14,47 +15,55 @@ def encrypt():
     encrypted_message = ""
     for char in text.lower():
         indexnum = alphabet.index(char)
+        indexnum = indexnum + shift
         if indexnum >= 26:
-            indexnum = indexnum - 26
-        encrypted_message += alphabet[(indexnum+shift)]
+            indexnum -= 26
+        encrypted_message += alphabet[(indexnum)]
     print(encrypted_message)    
+    return encrypted_message
         
-def decrypt():
+def decrypt(text, brute_shift, direction):
+    shift_allowed = True
+    direction = str(direction)
     decrypted_message = ""
-    text = input("type your message here:\n".lower())
     if direction == "decode" or direction == "2":
-        global shift_allowed
         shift_allowed = False
-    if shift_allowed == False:
+    if direction != "bruteforce" or direction != "3":
         while shift_allowed == False:  
-            shift = int(input("Type the shift number:\n"))
-            if shift >= 0 and shift <= 26:
+            brute_shift = int(input("Type the shift number:\n"))
+            if brute_shift >= 0 and brute_shift <= 26:
                 shift_allowed = True
-            shift = int(input("Type the shift number:\n"))
+
     for char in text:
-        indexnum = int((alphabet.index(char)-shift))
-        if indexnum <= 0:
+        indexnum = int((alphabet.index(char)-brute_shift))
+        if indexnum < 0:
             indexnum = indexnum + 26
         decrypted_message += alphabet[indexnum]
-    print(decrypted_message)
 
-def bruteforce():
-    shift = 0
-    for num in range(1,25):
-        shift +=1
-        decrypt()
+    print(f"{brute_shift} {decrypted_message}")
+
+def bruteforce(text, direction):
+    brute_shift = 0
+
+    for num in range(1,27):
+        brute_shift += 1
+        decrypt(text, brute_shift, direction)
 
 ###### Caeser Cipher ######
 
-while is_true == False:
-    direction = input("Please select a mode:\n1. Encode\n2. Decode\n3. Bruteforce\n".lower())
+while True:
+    direction = str(input("Please select a mode:\n1. Encode\n2. Decode\n3. Bruteforce\n".lower()))
     if direction == "encode" or direction == "1":
-        encrypt()
-        is_true = True
+        encrypt(get_message())
+        break
     elif direction == "decode" or direction == "2":
-        decrypt()
-        is_true = True
+        decrypt(get_message())
+        break
     elif direction == "bruteforce" or direction == "3":
-        bruteforce()
+        bruteforce(get_message(), direction)
+        break
     else: 
         print(f"Incorrect option!")
+
+
+   
