483d14420277db854fbbbe65c8a3022cb756434f
Jodyrobert Brent Garren <105601437+brentgarren@users.noreply.github.com>
2024-10-16 21:21:03 -0500

    Updated caesar_cipher.py
    added logo and name1 as well as made the menu look better

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,14 +1,29 @@
 import platform
 import os
-
-alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+logo = '''
+    ________                 __  __   _      
+   / ____/ ____  _________ _/ /_/ /_ ( )_____
+  / __/ / /_  / / ___/ __ `/ __/ __ \|// ___/
+ / /___/ / / /_/ /  / /_/ / /_/ / / / (__  ) 
+/_____/_/ /___/_/   \__,_/\__/_/ /_/ /____/  
+                                                                                  
+'''
+name1 = '''
+   ______                              _______       __             
+  / ________ ____  _________ ______   / ____(_____  / /_  ___  _____
+ / /   / __ `/ _ \/ ___/ __ `/ ___/  / /   / / __ \/ __ \/ _ \/ ___/
+/ /___/ /_/ /  __(__  / /_/ / /     / /___/ / /_/ / / / /  __/ /    
+\____/\__,_/\___/____/\__,_/_/      \____/_/ .___/_/ /_/\___/_/     
+                                          /_/                       
+'''
+alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def clear_console():
     if platform.system() == "Windows":
         os.system('cls')
 
 def get_message():
-    output = str(input("Message: "))
+    output = str(input("Message: ")).replace(" ", "")
     clear_console()
     return output
 def get_shift():
@@ -21,14 +36,14 @@ def encrypt(text):
     shift_allowed = False
     while shift_allowed == False:  
         shift = get_shift()
-        if shift >= 0 and shift <= 26:
+        if shift >= 0 and shift <= 25:
             shift_allowed = True
     encrypted_message = ""
     for char in text.lower():
         indexnum = alphabet.index(char)
         indexnum = indexnum + shift
-        if indexnum >= 26:
-            indexnum -= 26
+        if indexnum >= 25:
+            indexnum -= 25
         encrypted_message += alphabet[(indexnum)]
     print(encrypted_message)    
     return encrypted_message
@@ -42,13 +57,13 @@ def decrypt(text, brute_shift, direction=0):
     if not brute_shift:
         while shift_allowed == False:  
             brute_shift = get_shift()
-            if brute_shift >= 0 and brute_shift <= 26:
+            if brute_shift >= 0 and brute_shift <= 25:
                 shift_allowed = True
 
     for char in text:
         indexnum = int((alphabet.index(char)-brute_shift))
         if indexnum < 0:
-            indexnum = indexnum + 26
+            indexnum = indexnum + 25
         decrypted_message += alphabet[indexnum]
 
     print(f"{brute_shift} {decrypted_message}")
@@ -56,7 +71,7 @@ def decrypt(text, brute_shift, direction=0):
 def bruteforce(text, direction):
     brute_shift = 0
 
-    for num in range(1,27):
+    for num in range(1,26):
         brute_shift += 1
         decrypt(text, brute_shift, direction)
 
@@ -66,7 +81,8 @@ def bruteforce(text, direction):
 
 while True:
     clear_console()
-    direction = str(input("Please select a mode:\n1. Encode\n2. Decode\n3. Bruteforce\n".lower()))
+    print(f"{logo}{name1}")
+    direction = str(input("Please select a Mode:\n1. Encode\n2. Decode\n3. Bruteforce\n\nMode: ")).lower()
     clear_console()    
 
     if direction == "encode" or direction == "1":
