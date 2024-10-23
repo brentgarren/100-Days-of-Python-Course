6215cb4d3eb1abf558e90a69af1fcce42aa38680
Jodyrobert Brent Garren <brentgarren@yahoo.com>
2024-10-23 10:57:46 -0500

    number guessing game

---
+++ guessthenumber.py
@@ -0,0 +1,48 @@
+import random
+
+logo = '''
+   ___                  _   _          _  _            _             
+  / __|_  _ ___ ______ | |_| |_  ___  | \| |_  _ _ __ | |__  ___ _ _ 
+ | (_ | || / -_|_-<_-< |  _| ' \/ -_) | .` | || | '  \| '_ \/ -_) '_|
+  \___|\_,_\___/__/__/  \__|_||_\___| |_|\_|\_,_|_|_|_|_.__/\___|_|  
+                                                                     
+'''
+attempts = 0
+game_over = False
+difficulty_select = False
+correct_number = random.randint(1,100)
+guess = ""
+
+
+def gameplay(difficulty):
+    if difficulty == 'easy':
+        return 10
+    if difficulty == 'hard':
+        return 5
+
+print(logo)
+
+print(f"Welcome to the Number Guesssing Game!\nI'm thinking of a number between 1 and 100.")
+while difficulty_select == False:
+    difficulty = str(input(f"Choose a difficulty. Type 'easy' or 'hard': ").lower())
+    attempts = gameplay(difficulty)
+    if attempts > 0:
+        break
+while game_over == False:
+    guess = int(input(f"Guess a number between 1 and 100!: "))
+    if guess == correct_number:
+        print("You Win")
+        break
+    elif guess > correct_number:
+        print('To High!')
+        attempts -= 1
+        print(f"You have {attempts} remaining!")
+    elif guess < correct_number:
+        print(F'Too low!')
+        attempts -= 1
+        print(f"You have {attempts} remaining!")
+    else:
+        print("enter a valid number!")
+    if attempts == 0:
+        print(f"You losee the correct number was {correct_number}")    
+        break
