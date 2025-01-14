4bca6f96437e602c9a2f9eb84978aae6ba3de75c
Jodyrobert Brent Garren <brentgarren@yahoo.com>
2025-01-13 19:34:36 -0600

    say 23 complete

--- Intermediate_day15-44/Day 23 Capstone/main.py
+++ Intermediate_day15-44/Day 23 Capstone/main.py
@@ -4,11 +4,45 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+#Configures Screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
 
+#Classes
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+#Keybinds
+screen.onkeypress(key="w", fun=player.move_forward)
+
+#Game
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    scoreboard.update_scoreboard()
+    
+    car_manager.create_car()
+    car_manager.move_cars()
+    
+    
+    #detect collision between player and car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+           game_is_on = False
+           scoreboard.game_over() 
+           
+    #detect turtle has crossed road
+    if player.is_at_finsh():
+        player.go_to_start()
+        car_manager.increase_level()
+        scoreboard.increase_level()
+        
+
+
+
+
+screen.exitonclick()
--- Intermediate_day15-44/Day 23 Capstone/player.py
+++ Intermediate_day15-44/Day 23 Capstone/player.py
@@ -1,7 +1,33 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("turtle")
+        self.color("black")
+        self.go_to_start()
+        self.setheading(90)
+
+    def move_forward(self):
+        self.forward(10)
+        
+    def is_at_finsh(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        return False
+    
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+        
+        
+
